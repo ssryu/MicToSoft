a9dd26c321dc35223f29d4fa8321c8dea569d475
kme222yh <49933526+kme222yh@users.noreply.github.com>
2019-05-11 22:40:04 +0900

    create site file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MicToSoft</title>
+
+        <?php wp_head(); ?>
+    </head>
+    <body>
+
+        <h1>MicToSoft</h1>
+
+        <p>サンプル</p>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
