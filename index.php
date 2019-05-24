66fa61248d68a5930f04569b510166c32ee6dd92
SSRyu <48610088+SSRyu@users.noreply.github.com>
2019-05-24 12:02:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
         }
     </style>
     <script>
-        var path = "<?php echo get_template_directory_url();?>";
+        var path = "<?php echo get_template_directory_uri();?>";
     </script>
-    <script src="<?php echo get_template_directory_url();?>/FunkyBlocks/FunkyBlocks.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/FunkyBlocks/FunkyBlocks.js"></script>
 
     <?php wp_head(); ?>
 </head>
@@ -29,16 +29,16 @@
 
     <p>サンプル</p>
 
-    <audio src="<?php echo get_template_directory_url();?>/FunkyBlocks/sound.mp3" id="sound"></audio>
-    <audio src="<?php echo get_template_directory_url();?>/FunkyBlocks/letsgo.mp3" id="bgm"></audio>
+    <audio src="<?php echo get_template_directory_uri();?>/FunkyBlocks/sound.mp3" id="sound"></audio>
+    <audio src="<?php echo get_template_directory_uri();?>/FunkyBlocks/letsgo.mp3" id="bgm"></audio>
     <canvas id="canvas" width="800" height="600"></canvas>
-    <img id="START" src="<?php echo get_template_directory_url();?>/FunkyBlocks/start.png" onclick="go()"><br/>
-    <img id="bgimage" src="<?php echo get_template_directory_url();?>/FunkyBlocks/back.png" style="display:none" />
-    <img id="block0" src="<?php echo get_template_directory_url();?>/FunkyBlocks/block0.png" style="display:none" />
-    <img id="block1" src="<?php echo get_template_directory_url();?>/FunkyBlocks/block1.png" style="display:none" />
-    <img id="block2" src="<?php echo get_template_directory_url();?>/FunkyBlocks/block2.png" style="display:none" />
-    <img id="block3" src="<?php echo get_template_directory_url();?>/FunkyBlocks/block3.png" style="display:none" />
-    <img id="block4" src="<?php echo get_template_directory_url();?>/FunkyBlocks/block4.png" style="display:none" />
+    <img id="START" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/start.png" onclick="go()"><br/>
+    <img id="bgimage" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/back.png" style="display:none" />
+    <img id="block0" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/block0.png" style="display:none" />
+    <img id="block1" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/block1.png" style="display:none" />
+    <img id="block2" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/block2.png" style="display:none" />
+    <img id="block3" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/block3.png" style="display:none" />
+    <img id="block4" src="<?php echo get_template_directory_uri();?>/FunkyBlocks/block4.png" style="display:none" />
 
     <script type="text/javascript">
         FunkyBlocks.init();
