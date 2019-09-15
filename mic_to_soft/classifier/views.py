21405efd547561257c0c1ad4dd8a2fb02f9f4c86
SSRyu <jjkztok3@gmail.com>
2019-09-16 00:09:09 +0900

    add api sample

--- mic_to_soft/classifier/views.py
+++ mic_to_soft/classifier/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 def index(request):
     return render(request, 'classifier/index.html', context = {'test' : 'test text'})
+
+@csrf_exempt
+def api(request):
+    if request.method == "POST":
+        form = request.POST
+        return JsonResponse(
+            json.dumps(
+                {
+                    'req' : str(request),
+                    'data' : form,
+                    'text' : form['text'].split(" ")
+                }
+            ),
+            safe = False
+        )
