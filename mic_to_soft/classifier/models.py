4f2d80cda9b1d3ebd632959773e3930074689e4b
SSRyu <jjkztok3@gmail.com>
2019-10-03 07:47:13 +0900

    file upload system sample

--- mic_to_soft/classifier/models.py
+++ mic_to_soft/classifier/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Classifier(models.Model):
+    title = models.CharField(max_length = 200)
+
+    textdata = models.FileField(upload_to = 'textdata/')
+
+    # pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
