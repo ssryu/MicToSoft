53887bda847ec8f64b35c3cd7a51359f4b3adcd9
SSRyu <jjkztok3@gmail.com>
2019-12-27 22:10:18 +0900

    add celery

---
+++ mic_to_soft/mic_to_soft/celery.py
@@ -0,0 +1,19 @@
+# mic_to_soft/mic_to_soft/celery.py
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+settings = os.getenv(
+   "DJANGO_SETTINGS_MODULE", "mic_to_soft.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+
+app = Celery('mic_to_soft')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(['mic_to_soft'])
